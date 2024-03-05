02f97802c32095eb3012b4fe38ea455d54c96dc3
William Woodard <williamwoodard98@gmail.com>
2024-03-05 14:28:00 -0600

    Added 2 click events

--- scripts/haulers.js
+++ scripts/haulers.js
@@ -1,13 +1,39 @@
-import { getHaulingShips } from "./database.js"
+import { getHaulingShips, getShippingShips } from "./database.js"
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+
+        if (itemClicked.dataset.type === "hauler") {
+
+            const haulerId = itemClicked.dataset.id
+
+            let shippingCounter = 0
+
+            const shippingShips = getShippingShips()
+            for (const ship of shippingShips) {
+                if (parseInt(haulerId) === ship.haulerId) {
+                    shippingCounter++
+                }
+            }
+            
+        window.alert(`This hauler is carrying ${shippingCounter} shipping ships`)    
+        }
+    }
+) 
 
 export const HaulerList = () => {
     const haulers = getHaulingShips()
+   
 
     let haulerHTML = `<div class="haulerbox"><h2>Hauler List</h2><ul>`
 
     for (const hauler of haulers) {
-        haulerHTML += `<li>${hauler.name}</li>`
+        haulerHTML += `<li 
+        data-id="${hauler.id}"
+        data-type="hauler">
+        ${hauler.name}</li>`
     }
 
     haulerHTML += "</ul></div>"
--- scripts/shippingships.js
+++ scripts/shippingships.js
@@ -1,5 +1,27 @@
-import { getShippingShips } from "./database.js"
+import { getShippingShips, getHaulingShips } from "./database.js"
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+
+        if (itemClicked.dataset.type === "shippingship") {
+
+            const haulerId = itemClicked.dataset.haulerid
+            
+            let haulingShip = { name: "Incorrect" }
+            
+            const haulerObjects = getHaulingShips()
+            for (const hauler of haulerObjects) {
+                if (parseInt(haulerId) === hauler.id) {
+                    haulingShip = hauler.name
+                }
+            }
+           
+            window.alert(`${itemClicked.dataset.name} is being hauled by ${haulingShip}`)
+        }
+    }
+)
 
 export const ShippingShipList = () => {
     const shippingShips = getShippingShips()
@@ -7,7 +29,12 @@ export const ShippingShipList = () => {
     let shippingShipsHTML = `<div class="shippingshipbox"><h2>Shipping Ship List</h2><ul>`
 
     for (const shippingShip of shippingShips) {
-        shippingShipsHTML += `<li>${shippingShip.name}</li>`
+        shippingShipsHTML += `<li
+        data-type="shippingship"
+        data-id="${shippingShip.id}"
+        data-haulerid="${shippingShip.haulerId}"
+        data-name="${shippingShip.name}"
+        >${shippingShip.name}</li>`
     }
 
     shippingShipsHTML += "</ul></div>"
